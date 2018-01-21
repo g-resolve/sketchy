31c8a5f1dd6418ce14964aa8b282a4dcacc899bc
HolmesNY <g-resolve@andrezoon.com>
2018-01-20 21:00:19 -0500

    Adding app components

--- server/server.js
+++ server/server.js
@@ -2,12 +2,32 @@ const WebSocket = require('ws');
 const port = 80;
 const fs = require('fs');
 const app = require('express')();
-app.use(/\./, (req, res, next) => {
-  fs.createReadStream();
-});
-//app.use((req, res) => {
-//  res.end('Hello');
-//});
+const es6Renderer = require('express-es6-template-engine');
+const appPath = process.cwd() + '/../app';
+const paths = {css: appPath + '/css', views: appPath + '/views', scripts: appPath + '/scripts'}
+app.engine('html', es6Renderer);
+app.set('view engine', 'html');
+app.set('views', paths.views);
+
+app.route('/api')
+  .all((req, res) => {
+    res.end('Just the server here...');
+  })
+  .get((req, res) => {
+
+  })
+  .post((req, res) => {
+
+  })
+app.use((req,res,next) => {
+  if(req.path == '/')
+    app.render('index', (e, html) => res.status(200).end(html));
+  else
+    fs.exists(appPath + req.path, (e) => {
+      !e && res.status(404).end("Not Found");
+      e && fs.createReadStream(appPath + req.path).pipe(res);
+    });
+})
 const ws = new WebSocket.Server({server: app.listen(port, () => console.log("I'm listening"))});
 ws.on('connection', wsConn => {
   wsConn.on('message', data => {
